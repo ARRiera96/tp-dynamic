47236d195efe554d3c23387d837643f5b73bed11
Angel <rriera96@gmail.com>
2016-05-11 19:55:41 -0400

    updated index.js

--- models/index.js
+++ models/index.js
@@ -5,8 +5,11 @@ var Hotel = require('./hotel');
 var Restaurant = require('./restaurant');
 var Activity = require('./activity');
 
+//Setting up all of the associations
+
 Hotel.belongsTo(Place);
 Restaurant.belongsTo(Place);
 Activity.belongsTo(Place);
 
 module.exports = db;
+ 
