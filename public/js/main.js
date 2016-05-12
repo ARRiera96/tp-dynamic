50b31f43c554c30c81db21d2a84282a90d7d72ae
Angel <rriera96@gmail.com>
2016-05-12 16:14:58 -0400

    added functionality for adding and removing days

--- public/js/main.js
+++ public/js/main.js
@@ -1,91 +1,67 @@
-$(function initializeMap() {
+//Why are we wrapping this is jquery's $(), for IFEE?? 
 
-    var fullstackAcademy = new google.maps.LatLng(40.705086, -74.009151);
+$(function initializeMap (){
 
-    var styleArr = [{
-        featureType: 'landscape',
-        stylers: [{
-            saturation: -100
-        }, {
-            lightness: 60
-        }]
-    }, {
-        featureType: 'road.local',
-        stylers: [{
-            saturation: -100
-        }, {
-            lightness: 40
-        }, {
-            visibility: 'on'
-        }]
-    }, {
-        featureType: 'transit',
-        stylers: [{
-            saturation: -100
-        }, {
-            visibility: 'simplified'
-        }]
-    }, {
-        featureType: 'administrative.province',
-        stylers: [{
-            visibility: 'off'
-        }]
-    }, {
-        featureType: 'water',
-        stylers: [{
-            visibility: 'on'
-        }, {
-            lightness: 30
-        }]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'geometry.fill',
-        stylers: [{
-            color: '#ef8c25'
-        }, {
-            lightness: 40
-        }]
-    }, {
-        featureType: 'road.highway',
-        elementType: 'geometry.stroke',
-        stylers: [{
-            visibility: 'off'
-        }]
-    }, {
-        featureType: 'poi.park',
-        elementType: 'geometry.fill',
-        stylers: [{
-            color: '#b6c54c'
-        }, {
-            lightness: 40
-        }, {
-            saturation: -40
-        }]
-    }];
+  var fullstackAcademy = new google.maps.LatLng(40.705086, -74.009151);
 
-    var mapCanvas = document.getElementById('map-canvas');
+  var styleArr = [{
+    featureType: 'landscape',
+    stylers: [{ saturation: -100 }, { lightness: 60 }]
+  }, {
+    featureType: 'road.local',
+    stylers: [{ saturation: -100 }, { lightness: 40 }, { visibility: 'on' }]
+  }, {
+    featureType: 'transit',
+    stylers: [{ saturation: -100 }, { visibility: 'simplified' }]
+  }, {
+    featureType: 'administrative.province',
+    stylers: [{ visibility: 'off' }]
+  }, {
+    featureType: 'water',
+    stylers: [{ visibility: 'on' }, { lightness: 30 }]
+  }, {
+    featureType: 'road.highway',
+    elementType: 'geometry.fill',
+    stylers: [{ color: '#ef8c25' }, { lightness: 40 }]
+  }, {
+    featureType: 'road.highway',
+    elementType: 'geometry.stroke',
+    stylers: [{ visibility: 'off' }]
+  }, {
+    featureType: 'poi.park',
+    elementType: 'geometry.fill',
+    stylers: [{ color: '#b6c54c' }, { lightness: 40 }, { saturation: -40 }]
+  }];
 
-    var currentMap = new google.maps.Map(mapCanvas, {
-        center: fullstackAcademy,
-        zoom: 13,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        styles: styleArr
+  var mapCanvas = document.getElementById('map-canvas');
+
+  window.currentMap = new google.maps.Map(mapCanvas, {
+    center: fullstackAcademy,
+    zoom: 13,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    styles: styleArr
+  });
+
+  var iconURLs = {
+    hotel: '/images/lodging_0star.png',
+    restaurant: '/images/restaurant.png',
+    activity: '/images/star-3.png'
+  };
+
+  window.drawMarker= function(type, coords) {
+    var latLng = new google.maps.LatLng(coords[0], coords[1]);
+    var iconURL = iconURLs[type];
+    var marker = new google.maps.Marker({
+      icon: iconURL,
+      position: latLng
     });
+    marker.setMap(currentMap);
+    return marker; 
+  }
 
-    var iconURLs = {
-        hotel: '/images/lodging_0star.png',
-        restaurant: '/images/restaurant.png',
-        activity: '/images/star-3.png'
-    };
 
-    window.drawMarker = function (type, coords) {
-        var latLng = new google.maps.LatLng(coords[0], coords[1]);
-        var iconURL = iconURLs[type];
-        var marker = new google.maps.Marker({
-            icon: iconURL,
-            position: latLng
-        });
-        marker.setMap(currentMap);
-        return marker;
-    }
+  // drawMarker('hotel', [40.705137, -74.007624]);
+  // drawMarker('restaurant', [40.705137, -74.013940]);
+  // drawMarker('activity', [40.716291, -73.995315]);
+
 });
--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -3,10 +3,30 @@ var $restaurantSelect = $("#restaurant-choices");
 var $activitySelect = $("#activity-choices");
 var $itineraryLists = $('h4+.list-group');
 
-//Without wrapping in $(), this is a DOM element
+
 var $hotelList = $($itineraryLists[0]);
 var $restaurantList = $($itineraryLists[1]);
 var $activityList = $($itineraryLists[2]);
+
+//Mini/local Database
+var curDayIndex= 0;
+
+var Day= function(){
+  this.hotel= [];
+  this.restaurant= [];
+  this.activity= []; 
+}
+
+var Item = function($item, marker){
+  this.item = $item;
+  this.marker = marker;
+}
+
+var itineraryArr= [new Day()];
+//End
+
+
+//Starts filling in all the option panels 
 
 hotels.forEach(function (hotel) {
     $hotelSelect.append("<option>" + hotel.name + "</option>");
@@ -19,6 +39,9 @@ restaurants.forEach(function (restaurant) {
 activities.forEach(function (activity) {
     $activitySelect.append("<option>" + activity.name + "</option>");
 });
+
+//End
+
 
 function isItemInList(item, list) {
     return !!list.children().filter(function (_) {
@@ -56,17 +79,27 @@ function addItineraryItem(modelType) {
 
     if (!isItemInList($select.val(), $list)) {
         $list.append($itineraryItem);
+        //Gets the selected option's index 
         var selectionIndex = $select.prop('selectedIndex');
         var coords = modelArray[selectionIndex].place.location;
         var marker = drawMarker(modelType, coords);
-        addRemoveBtnHandler($removeButton, marker);
+        addRemoveBtnHandler($removeButton, marker, modelType);
+
+        //Add items to itinerary array
+        var item = new Item($itineraryItem, marker)
+        itineraryArr[curDayIndex][modelType].push(item); 
     }
 }
+
+
+//Event Handlers
+addDayBtnHandler($('.current-day'));
 
 $('#hotel-choices+button').on('click', function () {
     $hotelList.find('.remove').trigger('click')
     addItineraryItem('hotel');
-});
+
+    });
 
 $('#restaurant-choices+button').on('click', function () {
     addItineraryItem('restaurant');
@@ -76,11 +109,81 @@ $('#activity-choices+button').on('click', function () {
     addItineraryItem('activity');
 });
 
+$('#day-add').on('click', function(){
+    var $button= $('<button class="btn btn-circle day-btn">'+ $('.day-buttons').children().length +'</button>');
+    $(this).prev().after($button);
+    addDayBtnHandler($button);
+    itineraryArr.push(new Day());
+    console.log(itineraryArr);
+});
+//End  
+
+
 
 //Button Event Handlers
-function addRemoveBtnHandler($button, marker) {
+function addRemoveBtnHandler($button, marker, modelType) {
     $button.on('click', function () {
-        $(this).parent().remove();
+        var $parentDiv = $(this).parent()
+
+        // Remove the item from the local DB
+        var itemIndex = $parentDiv.parent().children().index($parentDiv)
+        itineraryArr[curDayIndex][modelType].splice(itemIndex, 1);
+
+        $parentDiv.remove();
         marker.setMap(null);
     })
 }
+
+//Add dayButton Handler
+function addDayBtnHandler($button){
+    $button.on('click', function(){
+      $('.current-day').removeClass('current-day');
+      $(this).addClass('current-day');
+      $('#day-title span').text("Day "+$(this).text())
+
+      $itineraryLists.children().remove();
+      Object.keys(itineraryArr[curDayIndex]).forEach(function(list){
+        itineraryArr[curDayIndex][list].forEach(function(item){
+          item.marker.setMap(null);
+        })
+      })
+      console.log("Over here" + $(this).text() );
+      curDayIndex = $(this).text() - 1;
+
+      itineraryArr[curDayIndex].hotel.forEach(function(item){
+        addRemoveBtnHandler(item.item.find('.remove'), item.marker, 'hotel')
+        $hotelList.append(item.item);
+        item.marker.setMap(currentMap);
+      });
+      itineraryArr[curDayIndex].restaurant.forEach(function(item){
+        addRemoveBtnHandler(item.item.find('.remove'), item.marker, 'restaurant')
+        $restaurantList.append(item.item);
+        item.marker.setMap(currentMap);
+      });
+      itineraryArr[curDayIndex].activity.forEach(function(item){
+        addRemoveBtnHandler(item.item.find('.remove'), item.marker, 'activity')
+        $activityList.append(item.item);
+        item.marker.setMap(currentMap);
+      });
+
+    })
+
+}
+
+
+
+
+$('#day-title').on('click','.remove',function(){
+  //erase the current day from the itineraryArr
+  itineraryArr.splice(curDayIndex, 1);
+  var numDays= itineraryArr.length; 
+  //remove and then redraw all the day buttons to reset them
+ $(".day-buttons").children().not('#day-add').remove();
+  for(var i=0; i< numDays; i++){
+    $("#day-add").before('<button class="btn btn-circle day-btn">'+ (i+1) +'</button>'); 
+
+  }
+    //trigger an event on the last day button.   
+
+})
+
