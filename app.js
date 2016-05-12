3feed55a7a93ea89518e8b5fed8369bed76b948c
Octowl <alsaff1987@gmail.com>
2016-05-11 21:33:58 -0400

    Beautify

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ app.engine('html', swig.renderFile);
 // logging and body-parsing
 app.use(morgan('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
 // statically serve front-end dependencies
 app.use('/bootstrap', express.static(__dirname + '/node_modules/bootstrap/dist'));
@@ -29,29 +31,29 @@ app.use(require('./routes'));
 
 // failed to catch req above means 404, forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // handle any errors
 app.use(function (err, req, res, next) {
-  console.error(err, err.stack);
-  res.status(err.status || 500);
-  res.render('error', {
-  	error: err
-  });
+    console.error(err, err.stack);
+    res.status(err.status || 500);
+    res.render('error', {
+        error: err
+    });
 });
 
 // listen on a port
 var port = 3000;
 app.listen(port, function () {
-	console.log('The server is listening closely on port', port);
-  db.sync()
-  .then(function () {
-    console.log('Synchronated the database');
-  })
-  .catch(function (err) {
-    console.error('Trouble right here in River City', err, err.stack);
-  });
+    console.log('The server is listening closely on port', port);
+    db.sync()
+        .then(function () {
+            console.log('Synchronated the database');
+        })
+        .catch(function (err) {
+            console.error('Trouble right here in River City', err, err.stack);
+        });
 });
